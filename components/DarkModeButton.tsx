764a3cda6ecc1d13a4311a844dd4895e82876486
unknown <jergra@yahoo.com>
2023-02-09 15:47:39 -0800

    dark mode button size

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -20,12 +20,12 @@ function DarkModeButton() {
         <div>
             {currentTheme === 'dark' ? (
                 <SunIcon
-                    className='h-8 w-8 cursor-pointer text-yellow-500'
+                    className='h-7 w-7 cursor-pointer text-yellow-500'
                     onClick={() => setTheme('light')}
                 />
             ) : (
                 <MoonIcon
-                    className='h-8 w-8 cursor-pointer text-gray-900'
+                    className='h-6 w-6 cursor-pointer text-gray-900'
                     onClick={() => setTheme('dark')}
                 />
             )}
